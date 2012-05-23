6770c21ab632d8df926d5388ae866ade7033b146
Pascal Gauthier <pgauthier@nihilisme.ca>
2012-05-23 11:39:56 -0400

    Add write to fifo

--- sbin/logelf.py
+++ sbin/logelf.py
@@ -22,25 +22,47 @@ __metaclass__ = type
 class Log:
     "Class of syslog log"
 
-    def __init__(self, amqp_server, virtualhost, credentials, amqp_exchange, ssl, syslog_socket, socket_buffer):
+    def __init__(self, amqp_server, virtualhost, credentials, amqp_exchange, ssl, syslog_fifo, syslog_socket, socket_buffer): 
         "Class initialisation"
 
         # Global class var
-        self.facilities = ("kernel messages", "user-level messages", "mail system", "system daemons", "security/authorization messages", "messages generated internally by syslogd", "line printer subsystem", "network news subsystem", "UUCP subsystem", "clock daemon", "security/authorization messages", "FTP daemon", "NTP daemon", "log audit", "log alert", "clock daemon", "local use 0", "local use 1", "local use 2", "local use 2", "local use 3", "local use 4", "local use 5", "local use 6", "local use 7")
+        self.facilities = ("kernel messages", "user-level messages", 
+                "mail system", "system daemons", 
+                "security/authorization  messages",
+                "messages generated internally by syslogd", 
+                "line printer subsystem", "network news subsystem",
+                "UUCP subsystem", "clock daemon", 
+                "security/authorization messages", "FTP daemon", 
+                "NTP subsystem", "log audit", "log alert", 
+                "clock daemon", "local use 0", "local use 1", 
+                "local use 2", "local use 2", "local use 3", 
+                "local use 4", "local use 5", "local use 6", "local use 7")
+        self.syslog_file = syslog_fifo 
         self.hostname = socket.gethostname()
         self.syslog_socket = syslog_socket
         self.socket_buffer = socket_buffer
         self.amqp_exchange = amqp_exchange
 
+        # Fifo initialisation
+        try:
+            self.syslog_fifo = open(syslog_fifo, 'w')
+        except Exception, err:
+            print "Fifo exception: %s" % (err)
+            sys.exit(1)
+
         # Socket initialisation
-        self.mysocket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-        self.mysocket.bind(syslog_socket)
+        try:
+            self.mysocket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+            self.mysocket.bind(syslog_socket)
+        except Exception, err:
+            print "Socket exception: %s" % (err)
+            sys.exit(1)
 
         # AMQP initialisation
         while True:
             try:
                 if ssl.get('enable') == "on":
-                    #self.ssl_options = { 'ca_certs': ssl_info.get('cacert'), 'certfile': ssl_info.get('cert'), 'keyfile': ssl_info.get('key') }
+                    #self.ssl_options = {'ca_certs': ssl_info.get('cacert'), 'certfile': ssl_info.get('cert'), keyfile': ssl_info.get('key')}
                     #self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=amqp_server, credentials=credentials, virtual_host=virtualhost, ssl=True, ssl_options=self.ssl_options))
                     print "AMQPS support broken right now (blame pika)... fallback to normal AMQP"
                     self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=amqp_server, credentials=credentials, virtual_host=virtualhost))
@@ -68,9 +90,10 @@ class Log:
                 self.close()
                 break
 
-    def write_to_fifo(self, fifo_file):
+    def __write_to_fifo__(self, gelf_msg):
         "Write to a fifo file"
-        pass
+
+        self.syslog_fifo.write(gelf_msg + "\n")
 
     def send(self, amqp_rkey):
         "Send syslog messages to AMQP server"
@@ -81,8 +104,11 @@ class Log:
                 # send to gelfify 
                 gelf_msg = self.gelfify(self.data)
                 print gelf_msg
+                # send to fifo
+                self.__write_to_fifo__(gelf_msg)
                 # send to amqp
-                self.channel.basic_publish(exchange=self.amqp_exchange, routing_key=amqp_rkey, body=gelf_msg)
+                self.channel.basic_publish(exchange=self.amqp_exchange, 
+                        routing_key=amqp_rkey, body=gelf_msg)
             except KeyboardInterrupt:
                 print "Keyboard interruption"
                 self.close()
@@ -102,13 +128,19 @@ class Log:
         self.short_msg = msg[4].replace(":", "")
         self.header = " ".join(msg[4:])
 
-        self.gelf_msg = { 'version': "1", 'timestamp': self.time, 'short_message': self.short_msg, 'full_message': self.header , 'host': self.hostname, 'level': self.severity, 'facility': self.facilities[int(self.facility)] }
+        self.gelf_msg = {'version': "1", 'timestamp': self.time, 'short_message': self.short_msg,
+                        'full_message': self.header , 'host': self.hostname, 'level': self.severity,
+                        'facility': self.facilities[int(self.facility)]}
 
         return json.dumps(self.gelf_msg)
     
     def close(self):
-        "Close the socket"
+        "Close the socket and fifo"
 
+        # Fifo
+        self.syslog_fifo.close()
+
+        # Socket
         self.mysocket.close()
         os.remove(self.syslog_socket)
 
@@ -128,6 +160,7 @@ def main():
     config = ConfigParser.RawConfigParser()
     config.readfp(config_fh)
 
+    syslog_fifo = config.get("syslog", "fifo")
     syslog_socket = config.get("syslog", "socket")
     socket_buffer = config.getint("syslog", "buffer")
     amqp_server = config.get("amqp", "server")
@@ -143,7 +176,7 @@ def main():
     ssl = { 'enable': ssl_enable, 'cacert': cacertfile, 'cert': certfile, 'key': keyfile }
     credentials = pika.PlainCredentials(username, password)
 
-    log = Log(amqp_server, virtualhost, credentials, amqp_exchange, ssl, syslog_socket, socket_buffer)
+    log = Log(amqp_server, virtualhost, credentials, amqp_exchange, ssl, syslog_fifo, syslog_socket, socket_buffer)
     log.send("log")
 
 if __name__ == '__main__':
